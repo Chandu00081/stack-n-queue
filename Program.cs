96a7526af4b578ffb748cfd9af603b8ab9f20840
chandu00081 <chandu00081@gmail.com>
2023-02-15 19:37:09 +0530

    creating stack

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(86);
+            stack.push(23);
+            stack.push(43);
+            stack.display();
+        }
+    }
+}
