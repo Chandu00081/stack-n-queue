6ae1f2bc3d193989e119091023685634c023b824
chandu00081 <chandu00081@gmail.com>
2023-02-15 19:52:02 +0530

    peek pop

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(86);
+            stack.push(23);
+            stack.push(43);
+            stack.display();
+            stack.peak();
+            stack.pop();
+            stack.peak();
+            stack.pop();
+            stack.peak();
+            stack.pop();
+            stack.display();
+
+
+        }
+    }
+}
