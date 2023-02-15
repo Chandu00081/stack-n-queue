7cc36546e18771f56d64ecc375e0e01414693e90
chandu00081 <chandu00081@gmail.com>
2023-02-15 20:31:27 +0530

    merged

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Queue queue = new Queue();
+            queue.Enqueue(34);
+            queue.Enqueue(56);
+            queue.Enqueue(13);
+            queue.display();
+
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+
+
+        }
+    }
+}
