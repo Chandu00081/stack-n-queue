72943142991bea01e897d465866025cd1f2ed697
chandu00081 <chandu00081@gmail.com>
2023-02-15 20:10:43 +0530

    creating queue

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Queue queue = new Queue();
+            queue.Enqueue(34);
+            queue.Enqueue(56);
+            queue.Enqueue(13);
+            queue.display();
+
+
+        }
+    }
+}
